e8e63de965567ba6d79a56f1b060cbe23406f564
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2024-10-03 18:56:47 +0200

    Add files via upload

---
+++ Week 2/Achterstevoren.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEncrypter
+{
+    internal class Achterstevoren : IEncrypter
+    {
+        public string decrypt(string text)
+        {
+            string result = "";
+
+            foreach(char c in text) {
+                result = c + result;
+            }
+
+            return result;
+        }
+
+        public string encrypt(string text)
+        {
+            return decrypt(text);
+        }
+
+        public override string ToString()
+        {
+            return "Achterstevoren";
+        }
+    }
+}
---
+++ Week 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using Spectre.Console;
+
+namespace TextEncrypter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<ITest> result = AnsiConsole.Prompt(
+                new MultiSelectionPrompt<ITest>()
+                    .Title("What are your [green]favorite fruits[/]?")
+                    .AddChoices<ITest>(new[]
+                    {
+                        new Test(),
+                        new Test()
+                    })
+                    .UseConverter(x => x.ToString())
+                );*/
+
+            string task = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Do you want to encrypt or decrypt")
+                    .AddChoices(new[] {
+                        "encrypt", "decrypt"
+                    }));
+
+            IEncrypter encrypter = AnsiConsole.Prompt(
+                new SelectionPrompt<IEncrypter>()
+                    .Title("Pick an encrypter")
+                    .AddChoices(new IEncrypter[] {
+                        new Achterstevoren(),
+                        new ShiftEncrypter(1),
+                    })
+                    .UseConverter(x => x.ToString()));
+
+            string text = AnsiConsole.Ask<string>("Input text: ");
+
+            if(task == "encrypt")
+            {
+                AnsiConsole.Write(encrypter.encrypt(text));
+            } else
+            {
+                AnsiConsole.Write(encrypter.decrypt(text));
+            }
+
+            return;
+
+        }
+    }
+}
