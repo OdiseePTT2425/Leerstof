bf95583e72856430e0afd6f7499831b8497e5e3a
jensbaetens <jens.baetens@ikdoeinformatica.be>
2024-11-05 15:12:37 +0100

    prep week 6

---
+++ Week 6/ExtraTechnieken/HigherLowerGame.Tests/HigherLowerAppTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherLowerGame.Tests
+{
+    [TestFixture]
+    public class HigherLowerAppTests
+    {
+        [Test]
+        public void GuessGuessNumber_WithGuessToHigh_ReturnsLower()
+        {
+           
+        }
+
+        [Test]
+        public void GuessGuessNumber_WithGuessToLow_ReturnsHigher()
+        {
+
+        }
+    }
+}
---
+++ Week 6/ExtraTechnieken/Person/PersonDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherLowerGame
+{
+    public partial class PersonDbContext : DbContext
+    {
+        public PersonDbContext()
+            : base("name=Persons")
+        {
+        }
+        
+
+        public virtual DbSet<Person> Persons { get; set; }
+    }
+}
